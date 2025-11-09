07befeb18b3d4bfd1545561cb211f4a01f2306bd
Muhammad Omer Baig <engromerbaig@gmail.com>
2025-11-09 17:00:47 +0500

    added

--- src/data/companyInfo.ts
+++ src/data/companyInfo.ts
@@ -1,15 +1,4 @@
 // data/companyInfo.ts
 export const companyInfo = {
-  name: "VZNX Technologies",
-  contact: {
-    address: "67, Aiwan e Tijarat Building, Aiwan e Tijarat Road, Karachi, Sindh, Pakistan",
-    addressLink: "https://maps.app.goo.gl/7jTazCmHGF2ZABoB9",
-    phone: "+923008250549",
-    website: "https://piecopk.com/",
-  },
-  developer: {
-    name: "Scons Tech",
-    website: "https://sconstech.com",
-    phone: "+923392136495",
-  },
+  name: "VZNX Technologies"
 };
